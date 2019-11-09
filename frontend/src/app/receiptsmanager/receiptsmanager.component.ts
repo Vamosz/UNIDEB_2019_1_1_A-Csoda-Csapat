7471fe4a66676cb4fc61622079d9c1cfacdea577
Annuci4 <anna.kuczik@gmail.com>
2019-11-09 17:55:02 +0100

    Recept manager oldal elkészítése -frontend

---
+++ frontend/src/app/receiptsmanager/receiptsmanager.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receiptsmanager',
+  templateUrl: './receiptsmanager.component.html',
+  styleUrls: ['./receiptsmanager.component.scss']
+})
+export class ReceiptsmanagerComponent{
+  title="Vámosi Patrik";
+  receipts = [
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Romesh Ranganathan's epic veg lasagne"
+  ];
+
+}
