147bf186c208d60d6ee2168b40da25510512e6ad
Patrik Vámosi <vamosipatrik@gmail.com>
2019-11-09 19:07:07 +0100

    Kereső mező beállitása - front end

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -7,27 +7,32 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent{
-title= 'Vámosi Patrik';
-urlOfPic ="";
-
-receipts = [
-  "Sir Patrick Stewart's mole enchiladas",
-  "Tom Walker's chicken & hot sauce gravy",
-  "Grilled chicken with charred pineapple salad",
-  "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
-  "Craig David's Grenadian baked chicken",
-  "Greg Davies' Thai green chicken curry",
-  "Roast leg of lamb with pancetta, sage & rosemary",
-  "Orlando Bloom's lamb shank tagine",
-  "Basic recipe for fresh egg pasta dough",
-  "Spaghetti with anchovies, dried chilli & pangrattato",
-  "Romesh Ranganathan's epic veg lasagne"
-];
+  title= 'Vámosi Patrik';
+  filter = "";
 
 
-drop(event: CdkDragDrop<string[]>) {
-  moveItemInArray(this.receipts, event.previousIndex, event.currentIndex);
-}
 
+  receipts = [
+    "Sir Patrick Stewart's mole enchiladas",
+    "Tom Walker's chicken & hot sauce gravy",
+    "Grilled chicken with charred pineapple salad",
+    "Crispy & sticky chicken thighs with squashed new potatoes & tomatoes",
+    "Craig David's Grenadian baked chicken",
+    "Greg Davies' Thai green chicken curry",
+    "Roast leg of lamb with pancetta, sage & rosemary",
+    "Orlando Bloom's lamb shank tagine",
+    "Basic recipe for fresh egg pasta dough",
+    "Spaghetti with anchovies, dried chilli & pangrattato",
+    "Romesh Ranganathan's epic veg lasagne"
+  ];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.receipts, event.previousIndex, event.currentIndex);
+  }
+
+  searchedFilter($event:any){
+    this.filter=$event.toElement.innerText;
+  }
 
 }
