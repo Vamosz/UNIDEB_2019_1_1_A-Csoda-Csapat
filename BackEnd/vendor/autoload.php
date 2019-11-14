484fa2c898e8e89abc904036c3fe32402c1378a4
Patrik Vámosi <vamosipatrik@gmail.com>
2019-11-14 12:04:33 +0100

    Új backend - Slim skeleton

--- BackEnd/vendor/autoload.php
+++ BackEnd/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2cb554ca0b27edf496bb88dc81fbf614::getLoader();
+return ComposerAutoloaderInit4238dfff2dd8dbdad341037436165570::getLoader();
